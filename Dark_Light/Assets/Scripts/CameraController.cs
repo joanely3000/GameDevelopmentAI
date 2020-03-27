a196695bec3e743d16e4aed5eb16cd91d837b7f0
joanely3000 <44837269+joanely3000@users.noreply.github.com>
2020-03-27 03:20:53 +0100

    Changes Almost updated to master
    Only thing left is create the cameras for the player

---
+++ Dark_Light/Assets/Scripts/CameraController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public List<GameObject> cameras = new List<GameObject>();
+
+    private int activeCamera = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cameras.Add(mainCamera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchCamera(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if(cameras.Count > 1)
+            {
+                SwitchCamera(1);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (cameras.Count > 2)
+            {
+                SwitchCamera(2);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (cameras.Count > 3)
+            {
+                SwitchCamera(3);
+            }
+        }
+    }
+
+    private void SwitchCamera(int index)
+    {
+        if (activeCamera != index)
+        {
+            cameras[activeCamera].GetComponent<AudioListener>().enabled = false;
+            cameras[activeCamera].SetActive(false);
+
+            cameras[index].SetActive(true);
+            cameras[index].GetComponent<AudioListener>().enabled = true;
+
+            activeCamera = index;
+        }
+    }
+
+    public void AddCamera(GameObject cam)
+    {
+        cameras.Add(cam);
+    }
+}
---
+++ Dark_Light/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offsetPosition;
+
+    private AudioListener audioListener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioListener = GetComponent<AudioListener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offsetPosition;
+        Vector3 rotation = Quaternion.AngleAxis(30, player.transform.right) * player.transform.forward;
+        transform.rotation = Quaternion.LookRotation(rotation);
+    }
+
+    public void SetAudioListenerActive(bool value)
+    {
+        audioListener.enabled = value;
+    }
+
+    public void SetPlayer(GameObject target)
+    {
+        player = target;
+    }
+}
